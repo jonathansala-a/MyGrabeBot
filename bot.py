27d2cddc244b6824020c28268c2d22300c754fb9
jonathansala-a <jonathansalazer@gmail.com>
2025-12-07 14:26:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,31 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
+APP_URL = os.getenv("APP_URL")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[
-        InlineKeyboardButton(
-            "Open Downloader",
-            web_app=WebAppInfo(url=os.getenv("APP_URL"))
-        )
-    ]]
+
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "Open Downloader",
+                web_app=WebAppInfo(url=APP_URL)
+            )
+        ]
+    ]
 
     await update.message.reply_text(
         "Welcome! Click below to open the downloader.",
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
-app = ApplicationBuilder().token(TOKEN).build()
+app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
